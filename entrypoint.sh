8339a242f7d77e28949108f8383cbea3dc0dd356
simon.c <simon@sig.tw>
2021-06-01 00:57:16 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ file_in_repo="$2"
 git_name="$3"
 git_email="$4"
 
-mkdir -p "$(dirname "$file_in_repo")"
 wget "$url" -O "$file_in_repo"
 
 git config user.name "$git_name"
@@ -13,4 +12,4 @@ git config user.email "$git_email"
 
 git add "$file_in_repo"
 git commit -m "Update $(basename "$file_in_repo") from $url"
-git push origin HEAD:main
+git push origin HEAD:master
